13cb90834baf346e123e08dba1a26a6b74acfc3d
Wonwoo Choi <onechoo1106@gmail.com>
2022-08-31 16:31:27 +1000

    wwc

--- src/app/user-dashboard/user-dashboard.component.ts
+++ src/app/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import{FormBuilder,FormGroup} from '@angular/forms'
+import { ApiService } from '../server/api.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-user-dashboard',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-dashboard.component.css']
 })
 export class UserDashboardComponent implements OnInit {
-
-  constructor() { }
+  formValue!:FormGroup;
+  //userModelObj:UserModel=new UserModel();
+  userData!:any;
+  constructor(private formbuilder:FormBuilder,private api
+    :ApiService) { }
 
   ngOnInit(): void {
+    this.formValue=this.formbuilder.group({
+      userName:[''],
+      Age:[''],
+      Birthday:[''],
+      Email:['']
+    })
+    this.getAllUser();
   }
+  getAllUser(){
+    this.api.getUser().subscribe(res=>{
+      this.userData=res;
+    })
+  }
+
 
 }
